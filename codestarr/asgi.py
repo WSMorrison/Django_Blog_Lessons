cee02b1e3843ebbacf68e3a5fe85941ba4b4c6f9
WSMorrison <willimo@gmail.com>
2023-01-18 16:33:25 +0000

    Initial commit with Django and psycopg2 installed, as well as appropriate Heroku and Elephant SQL links made.

---
+++ codestarr/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for codestarr project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestarr.settings')
+
+application = get_asgi_application()
