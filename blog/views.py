b9925b0aa9ca8ffb785ee34e65c3ebf4ddabdbe0
WSMorrison <willimo@gmail.com>
2023-01-19 18:00:52 +0000

    Add ability for users to like or unlike post as well as see if post is liked or not.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
 
 class PostDetail(View):
 
@@ -27,7 +30,7 @@ class PostDetail(View):
                 'comments': comments,
                 'commented': False,
                 'liked': liked,
-                'comment_form': CommentForm(),
+                'comment_form': CommentForm()
             },
         )
 
@@ -61,3 +64,15 @@ class PostDetail(View):
                 'comment_form': CommentForm(),
             },
         )
+
+class PostLike(View):
+
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
